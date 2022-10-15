d4bc2ef8baa8c4d6ce042889779cc4bd9fcf274d
Jorge Saavedra <jorgesaavedratoledo97@gmail.com>
2022-10-15 14:12:35 -0500

    Add Env Variables

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
-export const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
-export const BASE_URL = import.meta.env.VITE_BASE_URL;
-export const DUMMY_APP_ID = import.meta.env.VITE_DUMMY_APP_ID;
+export const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || process.env.VITE_GOOGLE_CLIENT_ID;
+export const BASE_URL = import.meta.env.VITE_BASE_URL || process.env.VITE_BASE_URL;
+export const DUMMY_APP_ID = import.meta.env.VITE_DUMMY_APP_ID || process.env.VITE_DUMMY_APP_ID;
